96fb1770f9b0620c0e4acfdc4d642f21b934b112
Raghava Lakshminarayana <raglaks94@gmail.com>
2019-03-20 22:54:55 -0600

    Updates

---
+++ precache-manifest.ce4d9372261117ed3692c1e64e490f6e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a76ba3102262d8d6c053",
+    "url": "/pokechallenge/static/css/main.9bd3459f.chunk.css"
+  },
+  {
+    "revision": "a76ba3102262d8d6c053",
+    "url": "/pokechallenge/static/js/main.45eda5f9.chunk.js"
+  },
+  {
+    "revision": "421c2352dd8c3db22075",
+    "url": "/pokechallenge/static/js/runtime~main.0c43c9d2.js"
+  },
+  {
+    "revision": "c5e02cf663f298ba25e5",
+    "url": "/pokechallenge/static/js/2.c6b99264.chunk.js"
+  },
+  {
+    "revision": "b012a6e08236e13f523479f5afcb2fd0",
+    "url": "/pokechallenge/index.html"
+  }
+];
